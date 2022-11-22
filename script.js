12703192c921cd96386960ba5bd9b802183d18c0
bhavana2552 <115105922+bhavana2552@users.noreply.github.com>
2022-11-22 18:07:01 +0530

    done

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //your code here
 function count(){
 var x = document.getElementById("wordCount").value;
-console.log(x);
+console.log(x)
+console.log(x.length);
 if(x.length!=0){
 
 
 var y = x.split(" ");
-console.log(y.length);
 
 
 document.getElementById("test").innerHTML=(y.length);
+}
+else{
+    document.getElementById("test").innerHTML=0;
 }
 }
