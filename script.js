7cf61555de0d8da2f9c892beacd73a9409b6d16d
bhavana2552 <115105922+bhavana2552@users.noreply.github.com>
2022-11-22 17:37:17 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your code here
+function count(){
+var x = document.getElementById("wordCount").value;
+console.log(x);
+var a = 0;
+for (var i=0;i<x.length;i++){
+    if(x[i]==" "){
+        a=a+1;
+    }
+
+}
+console.log(a);
+document.getElementById("test").innerHTML=(a+1);
+}
